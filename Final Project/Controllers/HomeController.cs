1f5f2bcc044ddc112b8816d6157b2771887a7156
samgehrlich <gehrlism@mail.uc.edu>
2021-11-29 13:40:40 -0500

    changes

--- Final Project/Controllers/HomeController.cs
+++ Final Project/Controllers/HomeController.cs
@@ -23,11 +23,9 @@ namespace Final_Project.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
+     
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
