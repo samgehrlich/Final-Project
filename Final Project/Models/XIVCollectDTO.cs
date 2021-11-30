3ddad9590f4d488243cd1c99878031eafabac122
Colin Vitols <54523046+vitolscolin@users.noreply.github.com>
2021-11-29 21:40:40 -0500

    Initial webpage design
    added api call to parse json data at controller
    returned image and sample text to my webpage

---
+++ Final Project/Models/XIVCollectDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Project.Models
+{
+    public class XIVCollectDTO
+    {
+        public string portraitLink { get; set; }
+        public string allMounts { get; set; }
+        public string totalMounts { get; set; }
+        public DateTime lastUpdated { get; set; }
+        public string allCards { get; set; }
+        public string totalCards { get; set; }
+    }
+
+}
+
